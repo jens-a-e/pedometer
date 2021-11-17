cc7a5e0edb9225d0b95ce41f7755ccb69b645f6a
jens <jens@lea.io>
2021-11-17 10:27:35 +0000

    Adding License info to code

--- pedometer.ino
+++ pedometer.ino
@@ -1,7 +1,36 @@
+/**
+ * Pedometer
+ *
+ * A step tracker using the algorithm of the Open Source C-Step-Counter by Anna
+ * Brondin & Marcus Nordstrom at Malmö University -
+ * https://github.com/MarcusNordstrom/C-Step-Counter
+ *
+ * Copyright (C) 2021  jens alexander ewald <jens@poetic.systems>
+ *
+ * This program is free software: you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License as published by the Free Software
+ * Foundation, either version 3 of the License, or (at your option) any later
+ * version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * this program.  If not, see <https://www.gnu.org/licenses/>.
+ *
+ * ------
+ *
+ * This project has received funding from the European Union’s Horizon 2020
+ * research and innovation programme under the Marie Skłodowska-Curie grant
+ * agreement No. 813508.
+ */
+
 #include <Arduino_LSM9DS1.h>
 
 extern "C" {
-#include "StepCountingAlgo.h"
+  #include "StepCountingAlgo.h"
 }
 
 
